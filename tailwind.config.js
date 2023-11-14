8a8383b522b761870004f3cf275cb48289650ebe
Tom Tamen <tomtamen@gmx.fr>
2023-11-14 15:42:15 +0100

    tailwind config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
         extend: {},
+        colors: {
+            DarkBlue: '#162061',
+            LightBlue: '#3ECADD',
+        },
     },
     plugins: [],
 };
