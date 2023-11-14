4a211474deed6ac9c540f9698d5787d71603d20f
RomainLAU <dev@romain-laurent.fr>
2023-11-14 11:27:26 +0100

    Add tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold">Bienvenue à l'Hopital</h1>
+            <h2 className="text-xl">Etapes de votre onboarding: </h2>
+            <ul className="flex flex-col gap-y-8 justify-center items-center">
+                <li className="bg-slate-400 rounded-md w-fit p-4">Récupérer votre ordinateur</li>
+                <li className="bg-slate-400 rounded-md w-fit p-4">Récupérer votre badge</li>
+                <li className="bg-slate-400 rounded-md w-fit p-4">Aller à la cantine</li>
+            </ul>
+        </div>
+    );
+}
