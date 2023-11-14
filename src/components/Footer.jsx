b9b320ca1b63e621c129c9ab38a4d7c071e90511
Denis Bucspun <denis.bucspun@edu.devinci.fr>
2023-11-14 18:36:32 +0100

    :lipstick: add active link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,24 +3,41 @@ import { ReactComponent as Home } from "../assets/Home.svg"
 import { ReactComponent as Maps } from "../assets/Maps.svg"
 import { ReactComponent as Profil } from "../assets/Profil.svg"
 
+const activeClassName = "isActive"
 
 
 export default function Footer({children}) {
     return (
             <footer className="flex justify-around content-center fixed bottom-0 w-screen">
-                <div className="flex justify-around content-center text-center w-full py-4 bg-gradient-to-t from-[#FFFFFF] from-%">
-                    <NavLink to="/" className="flex flex-col-reverse content-center items-center">
-                        <p className="text-center">Home</p>
-                        <Home/>
-                    </NavLink>
-                    <NavLink to="/map" className="flex flex-col-reverse content-center items-center">
-                        <p>Maps</p>    
-                        <Maps/>
-                    </NavLink>
-                    <NavLink to="/profil" className="flex flex-col-reverse content-center items-center">
-                        <p>Profil</p>
-                        <Profil/>
-                    </NavLink>
+                <div className="flex justify-around content-center text-center w-full py-4 bg-gradient-to-t from-[#FFFFFF]">
+                <NavLink 
+                    to="/" 
+                    className={({ isActive }) => 
+                        isActive ? `flex flex-col-reverse content-center items-center ${activeClassName}` : "flex flex-col-reverse content-center items-center"
+                    }
+                >
+                    <p className="text-center">Home</p>
+                    <Home/>
+                </NavLink>
+                <NavLink 
+                    to="/map" 
+                    className={({ isActive }) => 
+                        isActive ? `flex flex-col-reverse content-center items-center ${activeClassName}` : "flex flex-col-reverse content-center items-center"
+                    }
+                >
+                    <p>Maps</p>
+                    <Maps/>
+                </NavLink>
+
+                <NavLink 
+                    to="/profil" 
+                    className={({ isActive }) => 
+                        isActive ? `flex flex-col-reverse content-center items-center ${activeClassName}` : "flex flex-col-reverse content-center items-center"
+                    }
+                >
+                    <p>Profil</p>
+                    <Profil/>
+                </NavLink>
                 </div>
             </footer>
 
--- src/screen/Step.jsx
+++ src/screen/Step.jsx
@@ -10,7 +10,6 @@ export default function Step() {
             <li className="bg-slate-400 w-[320px] p-4 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-full text-center me-2 mb-2 text-[22px]">Récupérer votre ordinateur</li>
             <li className="bg-slate-400 w-[320px] p-4 bg-slate-400 p-4 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-full text-center me-2 mb-2 text-[22px]">Récupérer votre badge</li>
             <li className="bg-slate-400 w-[320px] p-4 bg-slate-400 p-4 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-full text-center me-2 mb-2 text-[22px]">Aller à la cantine</li>
-            <Link className="bg-slate-400 w-[320px] p-4 bg-slate-400 p-4 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-full text-center me-2 mb-2 text-[22px]" to="/map">Map</Link>
         </ul>
         <Footer></Footer>
     </div>
