4a2ce00e75d1ce1817ff06fb211c7bf97d789c57
Denis Bucspun <bucspun.d@gmail.com>
2023-11-14 16:57:21 +0100

    :construction: menu, router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Step from "./screen/Step";
 import Footer from "./components/Footer";
 import Map from "./screen/Map";
+import Profil from "./screen/Profil";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
@@ -10,6 +11,7 @@ export default function App() {
             <Routes>
                     <Route index element={<Step />} />
                     <Route path="map" element={<Map />} />
+                    <Route path="profil" element={<Profil />} />
             </Routes>
         </BrowserRouter>
     );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,28 @@
+import { NavLink } from "react-router-dom"
+import { ReactComponent as Home } from "../assets/Home.svg"
+import { ReactComponent as Maps } from "../assets/Maps.svg"
+import { ReactComponent as Profil } from "../assets/Profil.svg"
+
+
+
 export default function Footer({children}) {
     return (
-    <div className="fixed bottom-0">
-            {children}
-            <h1>fezfzefzaf</h1>
-    </div>
+            <footer className="flex justify-around content-center fixed bottom-0 w-screen">
+                <div className="flex justify-around content-center text-center w-full py-4 bg-gradient-to-t from-[#FFFFFF] from-%">
+                    <NavLink to="/" className="flex flex-col-reverse content-center items-center">
+                        <p className="text-center">Home</p>
+                        <Home/>
+                    </NavLink>
+                    <NavLink to="/map" className="flex flex-col-reverse content-center items-center">
+                        <p>Maps</p>    
+                        <Maps/>
+                    </NavLink>
+                    <NavLink to="/profil" className="flex flex-col-reverse content-center items-center">
+                        <p>Profil</p>
+                        <Profil/>
+                    </NavLink>
+                </div>
+            </footer>
+
     )
 }
