e65e7baf7931e44516722aaeebfa4b05e6fa43fb
Denis Bucspun <bucspun.d@gmail.com>
2023-11-15 15:40:06 +0100

    :sparkles: introduce modal components

--- src/screen/Map.jsx
+++ src/screen/Map.jsx
@@ -1,10 +1,21 @@
 import Footer from "../components/Footer";
+import Modal from "../components/Modal";
 
 export default function Map() {
+
+    const activeElement = 
+    {
+        title: 'Bâtiment K, Allée 4',
+        code: 1234,
+        distance: '200m',
+        time: '5min',
+    }
+
     return (
         <div className="flex justify-center align-center flex-col gap-4">
             <h1>Map</h1>
-            <Footer></Footer>
+            <Modal activeElement={activeElement}></Modal>
+            <Footer/>
     </div>
     )
 }
